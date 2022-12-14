9940bdce17472b6bd506cbc549dd3f80071ce92c
Miroslav Bajtoš <oss@bajtos.net>
2022-12-14 17:42:09 +0100

    deploy to Fly.io
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- l1-node.js
+++ l1-node.js
@@ -8,13 +8,17 @@ import { createNode, defaultNodeConfig, SaturnProtocols } from './lib/helpers.js
 const PeerIdString =
   'CiYAJAgBEiDluNfV2GAg/vDcFs8wNmBR4vs8cSr2vd9iGdTki+H6YxIkCAESIOW419XYYCD+8NwWzzA2YFHi+zxxKva932IZ1OSL4fpjGkQIARJA+PpMIiIRRZKRK/7rT4eZKmJpqpZEu2wHFtQcYMnuP0/luNfV2GAg/vDcFs8wNmBR4vs8cSr2vd9iGdTki+H6Yw=='
 
+const publicHostname = process.env.FLY_APP_NAME && `${process.env.FLY_APP_NAME}.fly.dev`
+let url
+
 const peerId = await createFromProtobuf(Buffer.from(PeerIdString, 'base64'))
 
 const node = await createNode({
   ...defaultNodeConfig,
   peerId,
   addresses: {
-    listen: ['/ip4/127.0.0.1/tcp/5000'],
+    listen: ['/ip4/0.0.0.0/tcp/3030'],
+    announce: publicHostname && [`/dns/${publicHostname}/tcp/3030/p2p/${peerId}`],
   },
 })
 await node.start()
@@ -32,21 +36,36 @@ const server = http.createServer(async (req, res) => {
   })
 
   async function handle() {
+    const peers = node.getConnections()
     const cid = req.url.slice(1) // remove the leading '/' character
-    // console.log('Serving CID', cid)
 
     if (req.method !== 'GET') {
       return replyError(405, `Method ${req.method} is not allowed.`)
     }
 
-    if (!cid) {
-      return replyError(404, `Not found: ${req.url} Missing CID in the requested path`)
+    if (req.url === '/') {
+      res.writeHead(200, { 'content-type': 'text/html' })
+      res.write(`
+<h1>Welcome to Saturn Link PoC</h1>
+<h2>Connected L2 nodes</h2>
+${peers.length ? peers.map((p) => `<p><code>${p.remotePeer}</code>`) : '<p>(none)'}
+<h2>Example requests</h2>
+<p><a href="${url}bafybeib36krhffuh3cupjml4re2wfxldredkir5wti3dttulyemre7xkni">${url}bafybeib36krhffuh3cupjml4re2wfxldredkir5wti3dttulyemre7xkni</a>
+<p><a href="${url}bafybeigj5lcgh3zm4mdiyherixy7q6n4k5idj3jetetxfjwhbbuqyksyem">${url}bafybeigj5lcgh3zm4mdiyherixy7q6n4k5idj3jetetxfjwhbbuqyksyem</a>
+`)
+      res.end()
+      return
     }
 
-    const peers = node.getConnections()
-    if (!peers.length) {
-      return replyError(500, 'No L2 nodes are connected.')
+    if (!cid) {
+      return replyError(404, `[ERROR] Not found: ${req.url} Missing CID in the requested path`)
     }
+
+    if (!peers.length) {
+      return replyError(500, '[ERROR] No L2 nodes are connected.')
+    }
+
+    console.log('Serving CID', cid)
 
     // TODO: use consistent hashing to select the L2 Node to ask for the content
     const targetPeer = peers[0].remotePeer
@@ -58,24 +77,26 @@ const server = http.createServer(async (req, res) => {
     }
     // console.log('Saturn content request', saturnReq)
     await stream.sink([Buffer.from(JSON.stringify(saturnReq))])
-    // console.log('Streaming back the response')
 
+    console.log('Streaming back the response')
     res.writeHead(200)
     for await (const chunk of stream.source) {
       res.write(chunk.subarray())
     }
     res.end()
+    console.log('Done.')
   }
 
   function replyError(statusCode, msg) {
+    console.log('Request rejected:', statusCode, msg)
     res.writeHead(statusCode, { 'content-type': 'text/plain' })
     res.write(msg + '\n')
     res.end()
   }
 })
-server.listen(3000, '127.0.0.1')
+server.listen(+process.env.PORT || 3000)
 await once(server, 'listening')
-const url = `http://127.0.0.1:${server.address().port}/`
+url = publicHostname ? `https://${publicHostname}/` : `http://127.0.0.1:${server.address().port}/`
 console.log('HTTP server listening on %s', url)
 console.log(
   'Example requests:\n  %s%s\n  %s%s',
